30909f75d78f1fe07a0d76eeb99da0c68a5b576f
TriggeRs <triggers@pop-os.localdomain>
2021-06-29 11:19:26 +0000

    implented printUserMenu, need to implent user actions methods

--- eclipse-workspace/ATM/src/atmproject/ATM.java
+++ eclipse-workspace/ATM/src/atmproject/ATM.java
@@ -25,7 +25,7 @@ public class ATM {
 			
 			curUser= ATM.mainMenuPrompt(bank,sc);
 			
-			//TODO ATM.printUserMenu(curUser,sc);
+			ATM.printUserMenu(curUser,sc);
 			
 		}
 
@@ -61,6 +61,47 @@ public class ATM {
 		}while(authUser==null);//continue loop until successful login
 		
 		return authUser;
+	}
+	
+	public static void printUserMenu(User theUser, Scanner sc) {
+		theUser.printAccountSummary();
+		
+		int choice;
+		
+		
+		do {
+			System.out.printf("Welcome %s, what would you like to do?", theUser.getFirstName());
+			System.out.println("   1)Show account transaction history");
+			System.out.println("   2)Withdraw");
+			System.out.println("   3)Deposit");
+			System.out.println("   4)Transfer");
+			System.out.println("   5)Quit");
+			System.out.println("Enter choice:");
+			choice=sc.nextInt();
+			
+			if(choice<1 || choice>5 || !sc.hasNextInt()) {
+				System.out.println("Invalid choice. Only numbers from 1-5.");
+			}
+			
+		}while(choice<1 || choice>5 || !sc.hasNextInt());
+		
+		switch (choice) {
+		case 1: 
+			ATM.showTransHistory(theUser,sc);
+		case 2:
+			ATM.withdrawlFunds(theUser,sc);
+		case 3:
+			ATM.depositFunds(theUser,sc);
+		case 4:
+			ATM.transferFunds(theUser,sc);
+		}
+		
+		//redisplay menu until user quits
+		if(choice!=5) {
+			ATM.printUserMenu(theUser, sc);
+		}
+		
+		
 	}
 	
 	
